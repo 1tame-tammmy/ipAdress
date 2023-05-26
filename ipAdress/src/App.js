fc3df324528d18ae1a68887d54c46f0e9aee6505
Tame Tammy <tamfor04@gmail.com>
2023-05-26 11:18:04 +0100

    Ready to deploy

---
+++ ipAdress/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import APIFetch from './components/APIFetch';
+
+
+function App() {
+  return (
+    <div className="App">
+      <APIFetch/>
+    </div>
+  );
+}
+
+export default App;
